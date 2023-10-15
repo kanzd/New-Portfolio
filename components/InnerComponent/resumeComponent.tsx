74c1f68d377baaf5b27214ab1c9d5bf1fa1a1281
KanishkDixit <kanishk.dixit@go-yubi.com>
2023-10-16 04:55:21 +0530

    resume issue fix

--- components/InnerComponent/resumeComponent.tsx
+++ components/InnerComponent/resumeComponent.tsx
@@ -25,8 +25,8 @@ export default function ResumeComponent() {
           }
         `}
             </style>
-            <Document file={"Resume.pdf"} renderMode='canvas'>
-                <Page pageIndex={0} />
+            <Document file={"Resume.pdf"} renderMode='svg'>
+                <Page scale={0.7} pageIndex={0} />
             </Document>
             <button className={Styles.PdfButton} onClick={handleDownload}>Download Resume</button>
             {/* <a href={"Resume.pdf"}></a> */}
